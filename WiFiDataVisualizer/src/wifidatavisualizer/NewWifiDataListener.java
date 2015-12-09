daa5331c3a1bf6a5d61aefd1e4e2aac89704df4d
jameslic <jlicata@LT-JLICATA.stc.syrres.com>
2015-12-09 16:28:43 -0500

    Added documentation

--- WiFiDataVisualizer/src/wifidatavisualizer/NewWifiDataListener.java
+++ WiFiDataVisualizer/src/wifidatavisualizer/NewWifiDataListener.java
@@ -1,29 +1,46 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Listener interface for classes to implement in handling Wifi data
+ *
  */
 package wifidatavisualizer;
 
+import java.awt.Point;
+
 /**
+ * An interface to be implemented by everyone interested in "New Wifi Data"
+ * events
  *
  * @author James Licata
  */
-import java.awt.Point;
-
-// An interface to be implemented by everyone interested in "Hello" events
 public interface NewWifiDataListener
 {
+   /**
+    * Main function for handling new wifi 2D point data
+    *
+    * @param newWifiPoint the 2D point for incoming data
+    * @param dataType     the algorithm type associated with the data point
+    */
    public void newWifiData(Point newWifiPoint, WifiDataType dataType);
 
+   /**
+    * Helper function to control listener to display only a given number of data
+    * points
+    *
+    * @param nPoints  The number of data points for the interface to supply
+    * @param dataType The algorithm type associated with the data point
+    * @return the number of points returned
+    */
    public int displayLastNPoints(int nPoints, WifiDataType dataType);
 
+   /**
+    * Enumeration for Wifi Alorithm indicators for data
+    */
    enum WifiDataType
    {
       TRILATERATION,
       TRIANGULATION,
       FINGERPRINTING,
       DEFAULT
-   }
+   }//WifiDataType
 
-}
+}//NewWifiDataListener
